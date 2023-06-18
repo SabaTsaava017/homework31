793819cb297ae068f93d749b136dcbb8bd5cb677
SabaTsaava017 <sabacaava7@gmail.com>
2023-06-18 17:36:20 +0400

    add script js

---
+++ script.js
@@ -0,0 +1,9 @@
+// 1. შექმენით ფუნქია, რომელიც არგუმენტად მიიღებს რიცხვების  მასივს და დააბრუნებს ჯამს;
+let myArray = [1, 2, 3, 4, 5];
+let result = calculateSum(myArray);
+console.log(result);
+
+//3.შექმენით ფუნქცია, რომელიც არგუმენტად მიიღებს რიცხვს და დააბრუნებს ამ რიცხვის კვადრატს 
+let myNumber = 5;
+let result1 = calculateSquare(myNumber);
+console.log(result1); 
